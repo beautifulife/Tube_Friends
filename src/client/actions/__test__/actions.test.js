35b708233265cc684352bb96e248790e839e1f5f
Beautifulife <davidkim1018@gmail.com>
2019-03-15 13:06:05 +0900

    Make tests of actions and reducers

---
+++ src/client/actions/__test__/actions.test.js
@@ -0,0 +1,352 @@
+/* eslint-disable no-undef */
+import * as actionCreaters from '../index';
+import * as Types from '../actionTypes';
+
+describe('action creaters', () => {
+  describe('about auth page function', () => {
+    test('authPageActivated is valid', () => {
+      const result = actionCreaters.authPageActivated();
+
+      expect(result).toEqual({
+        type: Types.AUTH_PAGE_ACTIVATED,
+        isLoginActive: true
+      });
+    });
+
+    test('authPageDeactivated is valid', () => {
+      const result = actionCreaters.authPageDeactivated();
+
+      expect(result).toEqual({
+        type: Types.AUTH_PAGE_DEACTIVATED,
+        isLoginActive: false
+      });
+    });
+
+    test('authRequestForbidden is valid', () => {
+      const result = actionCreaters.authRequestForbidden();
+
+      expect(result).toEqual({
+        type: Types.AUTH_REQUEST_FORBIDDEN,
+        isLoginActive: true
+      });
+    });
+  });
+
+  describe('about create story function', () => {
+    test('createStoryComplete is valid', () => {
+      const result = actionCreaters.createStoryComplete();
+
+      expect(result).toEqual({
+        type: Types.CREATE_STORY_COMPLETE,
+        isLoading: false
+      });
+    });
+
+    test('createStoryError is valid', () => {
+      const result = actionCreaters.createStoryError();
+
+      expect(result).toEqual({
+        type: Types.CREATE_STORY_ERROR,
+        isLoading: false
+      });
+    });
+
+    test('createStoryRequested is valid', () => {
+      const result = actionCreaters.createStoryRequested();
+
+      expect(result).toEqual({
+        type: Types.CREATE_STORY_REQUESTED,
+        isLoading: true
+      });
+    });
+  });
+
+  describe('about fetch categories function', () => {
+    test('fetchCategoriesComplete is valid', () => {
+      const categories = ['test'];
+      const result = actionCreaters.fetchCategoriesComplete(categories);
+
+      expect(result).toEqual({
+        type: Types.FETCH_CATEGORIES_COMPLETE,
+        categories
+      });
+    });
+
+    test('fetchCategoriesError is valid', () => {
+      const result = actionCreaters.fetchCategoriesError();
+
+      expect(result).toEqual({
+        type: Types.FETCH_CATEGORIES_ERROR
+      });
+    });
+
+    test('fetchCategoriesRequested is valid', () => {
+      const result = actionCreaters.fetchCategoriesRequested();
+
+      expect(result).toEqual({
+        type: Types.FETCH_CATEGORIES_REQUESTED
+      });
+    });
+  });
+
+  describe('about fetch stories function', () => {
+    test('fetchStoriesComplete is valid', () => {
+      const stories = ['test'];
+      const sortType = ['list'];
+      const category = 'test';
+      const page = 1;
+      const result = actionCreaters.fetchStoriesComplete(
+        stories,
+        sortType,
+        category,
+        page
+      );
+
+      expect(result).toEqual({
+        type: Types.FETCH_STORIES_COMPLETE,
+        isLoading: false,
+        stories,
+        sortType,
+        category,
+        page
+      });
+    });
+
+    test('fetchStoriesError is valid', () => {
+      const result = actionCreaters.fetchStoriesError();
+
+      expect(result).toEqual({
+        type: Types.FETCH_STORIES_ERROR,
+        isLoading: false
+      });
+    });
+
+    test('fetchStoriesRequested is valid', () => {
+      const result = actionCreaters.fetchStoriesRequested();
+
+      expect(result).toEqual({
+        type: Types.FETCH_STORIES_REQUESTED,
+        isLoading: true
+      });
+    });
+  });
+
+  describe('about fetch story function', () => {
+    test('fetchStoryComplete is valid', () => {
+      const story = {story: 'story'};
+      const result = actionCreaters.fetchStoryComplete(story);
+
+      expect(result).toEqual({
+        type: Types.FETCH_STORY_COMPLETE,
+        isLoading: false,
+        story
+      });
+    });
+
+    test('fetchStoryError is valid', () => {
+      const result = actionCreaters.fetchStoryError();
+
+      expect(result).toEqual({
+        type: Types.FETCH_STORY_ERROR,
+        isLoading: false
+      });
+    });
+
+    test('fetchStoryRequested is valid', () => {
+      const result = actionCreaters.fetchStoryRequested();
+
+      expect(result).toEqual({
+        type: Types.FETCH_STORY_REQUESTED,
+        isLoading: true
+      });
+    });
+  });
+
+  describe('about like toggle function', () => {
+    test('likeToggleComplete is valid', () => {
+      const likeAction = 'add';
+      const storyId = '1234';
+      const user = { userId: 'userId' };
+      const result = actionCreaters.likeToggleComplete(likeAction, storyId, user);
+
+      expect(result).toEqual({
+        type: Types.LIKE_TOGGLE_COMPLETE,
+        isLoading: false,
+        likeAction,
+        storyId,
+        user
+      });
+    });
+
+    test('likeToggleError is valid', () => {
+      const result = actionCreaters.likeToggleError();
+
+      expect(result).toEqual({
+        type: Types.LIKE_TOGGLE_ERROR,
+        isLoading: false
+      });
+    });
+
+    test('likeToggleRequested is valid', () => {
+      const result = actionCreaters.likeToggleRequested();
+
+      expect(result).toEqual({
+        type: Types.LIKE_TOGGLE_REQUESTED,
+        isLoading: true
+      });
+    });
+  });
+
+  describe('about log in function', () => {
+    test('logInComplete is valid', () => {
+      const accessToken = 'token';
+      const user = {
+        photoURL: './photo.com',
+        subscribe: ['userId'],
+        subscriber: ['userId'],
+        uid: '1234',
+        userId: '1234',
+        username: 'name'
+      };
+      const result = actionCreaters.logInComplete(user, accessToken);
+
+      expect(result).toEqual({
+        type: Types.LOG_IN_COMPLETE,
+        isLoading: false,
+        isLoginActive: false,
+        isUserLoggedIn: true,
+        accessToken,
+        photoURL: user.photoURL,
+        subscribe: user.subscribe,
+        subscriber: user.subscriber,
+        uid: user.uid,
+        userId: user._id,
+        username: user.username
+      });
+    });
+
+    test('logInError is valid', () => {
+      const result = actionCreaters.logInError();
+
+      expect(result).toEqual({
+        type: Types.LOG_IN_ERROR,
+        isLoading: false
+      });
+    });
+
+    test('logInRequested is valid', () => {
+      const result = actionCreaters.logInRequested();
+
+      expect(result).toEqual({
+        type: Types.LOG_IN_REQUESTED,
+        isLoading: true
+      });
+    });
+  });
+
+  describe('about log out function', () => {
+    test('logOutComplete is valid', () => {
+      const result = actionCreaters.logOutComplete();
+
+      expect(result).toEqual({
+        type: Types.LOG_OUT_COMPLETE
+      });
+    });
+  });
+
+  describe('about toggle menu function', () => {
+    test('menuToggle is valid', () => {
+      const isMenuActive = true;
+      const result = actionCreaters.menuToggle(isMenuActive);
+
+      expect(result).toEqual({
+        type: Types.MENU_TOGGLE,
+        isMenuActive
+      });
+    });
+  });
+
+  describe('about toggle profile function', () => {
+    test('profileToggle is valid', () => {
+      const isProfileActive = true;
+      const result = actionCreaters.profileToggle(isProfileActive);
+
+      expect(result).toEqual({
+        type: Types.PROFILE_TOGGLE,
+        isProfileActive
+      });
+    });
+  });
+
+  describe('about search stories function', () => {
+    test('searchStoriesComplete is valid', () => {
+      const stories = ['story'];
+      const page = 1;
+      const result = actionCreaters.searchStoriesComplete(stories, page);
+
+      expect(result).toEqual({
+        type: Types.SEARCH_STORIES_COMPLETE,
+        isLoading: false,
+        stories,
+        page
+      });
+    });
+
+    test('searchStoriesError is valid', () => {
+      const result = actionCreaters.searchStoriesError();
+
+      expect(result).toEqual({
+        type: Types.SEARCH_STORIES_ERROR,
+        isLoading: false
+      });
+    });
+
+    test('searchStoriesRequested is valid', () => {
+      const result = actionCreaters.searchStoriesRequested();
+
+      expect(result).toEqual({
+        type: Types.SEARCH_STORIES_REQUESTED,
+        isLoading: true
+      });
+    });
+  });
+
+  describe('about subscription toggle function', () => {
+    test('subscriptionToggleComplete is valid', () => {
+      const subscribe = ['userId'];
+      const result = actionCreaters.subscriptionToggleComplete(subscribe);
+
+      expect(result).toEqual({
+        type: Types.SUBSCRIPTION_TOGGLE_COMPLETE,
+        isLoading: false,
+        subscribe
+      });
+    });
+
+    test('subscriptionToggleError is valid', () => {
+      const result = actionCreaters.subscriptionToggleError();
+
+      expect(result).toEqual({
+        type: Types.SUBSCRIPTION_TOGGLE_ERROR,
+        isLoading: false
+      });
+    });
+
+    test('subscriptionToggleRequested is valid', () => {
+      const result = actionCreaters.subscriptionToggleRequested();
+
+      expect(result).toEqual({
+        type: Types.SUBSCRIPTION_TOGGLE_REQUESTED,
+        isLoading: true
+      });
+    });
+  });
+});
+
+describe('action types', () => {
+  Object.keys(Types).forEach((action) => {
+    test(`${action} is valid`, () => {
+      expect(action).toBe(Types[action]);
+    });
+  });
+});
---
+++ src/client/reducers/__test__/reducers.test.js
@@ -0,0 +1,534 @@
+/* eslint-disable no-undef */
+import rootReducer from '../index';
+import * as Types from '../../actions/actionTypes';
+
+const createInitialState = () => {
+  return {
+    isLoginActive: false,
+    isMenuActive: false,
+    isProfileActive: false,
+    isUserLoggedIn: false,
+    isLoading: false,
+    accessToken: '',
+    categories: [],
+    photoURL: '',
+    page: 0,
+    stories: [],
+    story: {},
+    subscribe: [],
+    subscriber: [],
+    userId: '',
+    uid: '',
+    username: ''
+  };
+};
+
+describe('reducers validate', () => {
+  test('initial state validate', () => {
+    const state = createInitialState();
+    const result = rootReducer(undefined, {});
+
+    expect(result).toEqual(state);
+  });
+
+  describe('when action type about auth is', () => {
+    test('AUTH_PAGE_ACTIVATED', () => {
+      const action = {
+        type: Types.AUTH_PAGE_ACTIVATED,
+        isLoginActive: true
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoginActive = true;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('AUTH_PAGE_DEACTIVATED', () => {
+      const action = {
+        type: Types.AUTH_PAGE_DEACTIVATED,
+        isLoginActive: false
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoginActive = false;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('AUTH_REQUEST_FORBIDDEN', () => {
+      const action = {
+        type: Types.AUTH_REQUEST_FORBIDDEN,
+        isLoginActive: true
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoginActive = true;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+  });
+
+  describe('when action type about creating story is', () => {
+    test('CREATE_STORY_COMPLETE', () => {
+      const action = {
+        type: Types.CREATE_STORY_COMPLETE,
+        isLoading: false
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = false;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('CREATE_STORY_ERROR', () => {
+      const action = {
+        type: Types.CREATE_STORY_ERROR,
+        isLoading: false
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = false;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('CREATE_STORY_REQUESTED', () => {
+      const action = {
+        type: Types.CREATE_STORY_REQUESTED,
+        isLoading: true
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = true;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+  });
+
+  describe('when action type about fetching categories is', () => {
+    test('FETCH_CATEGORIES_COMPLETE', () => {
+      const categories = ['test'];
+      const action = {
+        type: Types.FETCH_CATEGORIES_COMPLETE,
+        categories
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.categories = categories;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+      expect(result.categories).toBe(state.categories);
+    });
+
+    test('FETCH_CATEGORIES_ERROR', () => {
+      const action = {
+        type: Types.FETCH_CATEGORIES_ERROR
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      expect(result).not.toBe(state);
+    });
+
+    test('FETCH_CATEGORIES_REQUESTED', () => {
+      const action = {
+        type: Types.FETCH_CATEGORIES_REQUESTED
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      expect(result).not.toBe(state);
+    });
+  });
+
+  describe('when action type about fetching stories is', () => {
+    test('FETCH_STORIES_COMPLETE', () => {
+      const stories = ['test'];
+      const action = {
+        type: Types.FETCH_STORIES_COMPLETE,
+        stories,
+        sortType: 'list',
+        category: 'test',
+        page: 1,
+        isLoading: false
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.stories = stories;
+      state.sortType = 'list';
+      state.category = 'test';
+      state.page = 1;
+      state.isLoading = false;
+
+      expect(result).toEqual(state);
+      expect(result.stories).toBe(state.stories);
+      expect(result).not.toBe(state);
+    });
+
+    test('FETCH_STORIES_ERROR', () => {
+      const action = {
+        type: Types.FETCH_STORIES_ERROR,
+        isLoading: false
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = false;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('FETCH_STORIES_REQUESTED', () => {
+      const action = {
+        type: Types.FETCH_STORIES_REQUESTED,
+        isLoading: true
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = true;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+  });
+
+  describe('when action type about fetching story is', () => {
+    test('FETCH_STORY_COMPLETE', () => {
+      const story = { story: 'story' };
+      const action = {
+        type: Types.FETCH_STORY_COMPLETE,
+        isLoading: false,
+        story
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.story = story;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+      expect(result.story).toBe(state.story);
+    });
+
+    test('FETCH_STORY_ERROR', () => {
+      const action = {
+        type: Types.FETCH_STORY_ERROR,
+        isLoading: false
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = false;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('FETCH_STORY_REQUESTED', () => {
+      const action = {
+        type: Types.FETCH_STORY_REQUESTED,
+        isLoading: true
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = true;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+  });
+
+  describe('when action type about authenticate is', () => {
+    test('LOG_IN_COMPLETE', () => {
+      const subscribe = ['test subscribe'];
+      const subscriber = ['test subscriber'];
+      const action = {
+        type: Types.LOG_IN_COMPLETE,
+        isLoading: false,
+        isLoginActive: false,
+        isUserLoggedIn: true,
+        accessToken: 'test token',
+        photoURL: 'test photoUrl',
+        subscribe,
+        subscriber,
+        uid: 'test uid',
+        userId: 'test userId',
+        username: 'test username'
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = false;
+      state.isLoginActive = false;
+      state.isUserLoggedIn = true;
+      state.accessToken = 'test token';
+      state.photoURL = 'test photoUrl';
+      state.subscribe = subscribe;
+      state.subscriber = subscriber;
+      state.uid = 'test uid';
+      state.userId = 'test userId';
+      state.username = 'test username';
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+      expect(result.subscribe).toBe(state.subscribe);
+      expect(result.subscriber).toBe(state.subscriber);
+    });
+
+    test('LOG_IN_ERROR', () => {
+      const action = {
+        type: Types.LOG_IN_ERROR,
+        isLoading: false
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = false;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('LOG_IN_REQUESTED', () => {
+      const action = {
+        type: Types.LOG_IN_REQUESTED,
+        isLoading: true
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = true;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('LOG_OUT_COMPLETE', () => {
+      const action = {
+        type: Types.LOG_OUT_COMPLETE
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+  });
+
+  describe('when action type about like toggling is', () => {
+    test('LIKE_TOGGLE_COMPLETE', () => {
+      const action = {
+        type: Types.LIKE_TOGGLE_COMPLETE,
+        isLoading: false,
+        likeAction: 'add',
+        storyId: 'test storyId',
+        user: { _id: 'test userId' }
+      };
+      const state = createInitialState();
+
+      state.stories = [{ _id: 'test storyId', like: [] }];
+      state.story = { _id: 'test storyId', like: [] };
+
+      let result = rootReducer(state, action);
+
+      expect(result.categories).toEqual(state.categories);
+      expect(result.categories).not.toBe(state.categories);
+      expect(result.isLoading).toBe(action.isLoading);
+      expect(result.stories[0].like[0]).toBe(action.user);
+      expect(result.story.like[0]).toBe(action.user);
+
+      action.storyId = 'different test userId';
+      action.user = { _id: 'wrong userId' };
+      result = rootReducer(result, action);
+
+      expect(result.stories[0].like[1]).toBeUndefined();
+      expect(result.story.like[1]).toBeUndefined();
+
+      action.likeAction = 'remove';
+      result = rootReducer(result, action);
+
+      expect(result.stories[0].like).toHaveLength(1);
+      expect(result.story.like).toHaveLength(1);
+
+      action.storyId = 'test storyId';
+      action.user = { _id: 'test userId' };
+      result = rootReducer(result, action);
+
+      expect(result.stories[0].like).toHaveLength(0);
+      expect(result.story.like).toHaveLength(0);
+    });
+
+    test('LIKE_TOGGLE_ERROR', () => {
+      const action = {
+        type: Types.LIKE_TOGGLE_ERROR,
+        isLoading: false
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = false;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('LIKE_TOGGLE_REQUESTED', () => {
+      const action = {
+        type: Types.LIKE_TOGGLE_REQUESTED,
+        isLoading: true
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = true;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+  });
+
+  describe('when action type about menu toggling is', () => {
+    test('MENU_TOGGLE', () => {
+      const action = {
+        type: Types.MENU_TOGGLE,
+        isMenuActive: true
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isMenuActive = true;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+  });
+
+  describe('when action type about profile toggling is', () => {
+    test('PROFILE_TOGGLE', () => {
+      const action = {
+        type: Types.PROFILE_TOGGLE,
+        isProfileActive: true
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isProfileActive = true;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+  });
+
+  describe('when action type about searching story is', () => {
+    test('SEARCH_STORIES_COMPLETE', () => {
+      const action = {
+        type: Types.SEARCH_STORIES_COMPLETE,
+        isLoading: false,
+        stories: [{story: 'test story'}],
+        page: 1
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.stories = [{story: 'test story'}];
+      state.page = 1;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('SEARCH_STORIES_ERROR', () => {
+      const action = {
+        type: Types.SEARCH_STORIES_ERROR,
+        isLoading: false
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = false;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('SEARCH_STORIES_REQUESTED', () => {
+      const action = {
+        type: Types.SEARCH_STORIES_REQUESTED,
+        isLoading: true
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = true;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+  });
+
+  describe('when action type about subscription toggling is', () => {
+    test('SUBSCRIPTION_TOGGLE_COMPLETE', () => {
+      const action = {
+        type: Types.SUBSCRIPTION_TOGGLE_COMPLETE,
+        isLoading: false,
+        subscribe: [{user: 'test user'}]
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = false;
+      state.subscribe = [{user: 'test user'}];
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('SUBSCRIPTION_TOGGLE_ERROR', () => {
+      const action = {
+        type: Types.SUBSCRIPTION_TOGGLE_ERROR,
+        isLoading: false
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = false;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+
+    test('SUBSCRIPTION_TOGGLE_REQUESTED', () => {
+      const action = {
+        type: Types.SUBSCRIPTION_TOGGLE_REQUESTED,
+        isLoading: true
+      };
+      const state = createInitialState();
+      const result = rootReducer(state, action);
+
+      state.isLoading = true;
+
+      expect(result).toEqual(state);
+      expect(result).not.toBe(state);
+    });
+  });
+});
